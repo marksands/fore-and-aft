16a622f0a8637fd1e57b393b69cb5ef873ac1863
Mark Sands <marksands07@gmail.com>
2011-01-23 03:02:22 -0600

    cleanup

--- main.cpp
+++ main.cpp
@@ -9,7 +9,9 @@
 
 #pragma GCC diagnostic ignored "-Wwrite-strings"
 
-typedef unsigned int u_int;
+typedef unsigned int u_int16;
+typedef long int int16;
+typedef unsigned long int u_int32;
 typedef long int int32;
 
 /*
@@ -64,6 +66,10 @@ int main(int argc, char* argv[])
     << std::endl;
 
   dfs();
+
+  #if defined(__WIN32__) || defined(_WIN32)
+    system("pause");
+  #endif
 
   return 0;
 }
@@ -156,12 +162,9 @@ void dfs()
     if (!n.visited){
       n.visited = true;
 
-      // for each state push node/state onto stack
-
       // multiplication is expensive
       int32 size2 = size*size;
-  
-      std::cout << "trying 1 and 2" << std::endl;    
+
       if ((n.empty - 1 >= 0) && (n.state[n.empty-1] == RED)) {
       // make a new state left
         Node newState(n.state, n.chargrid, n.empty-1, false );
@@ -169,10 +172,11 @@ void dfs()
         newState.state[n.empty] = newState.chargrid[n.empty] = RED;
 
           // found it!
-        if( newState.chargrid == goalstring )
+        if( newState.chargrid == goalstring ) {
+          PrintGrid( newState.chargrid );
           break;
+        }
         if( !lookup.isThere(newState.chargrid) ) {
-          std::cout << "1st" << std::endl;
           lookup.insertEntry(newState.chargrid);
           PrintGrid( newState.chargrid );
           s.push(newState);
@@ -184,27 +188,28 @@ void dfs()
           newState.state[n.empty-2] = newState.chargrid[n.empty-2] = START;
           newState.state[n.empty] = newState.chargrid[n.empty] = RED;
             // found it!
-          if( newState.chargrid == goalstring )
+          if( newState.chargrid == goalstring ) {
+            PrintGrid( newState.chargrid );
             break;
+          }
           if( !lookup.isThere(newState.chargrid) ) {
-            std::cout << "2nd" << std::endl;
             lookup.insertEntry(newState.chargrid);
             PrintGrid( newState.chargrid );
             s.push(newState);
           }
         }
 
-      std::cout << "trying 3 and 4" << std::endl;
       if ((n.empty + 1 < size2) && (n.state[n.empty+1] == BLUE)) {
         // make a new state right
         Node newState(n.state, n.chargrid, n.empty+1, false );
         newState.state[n.empty+1] = newState.chargrid[n.empty+1] = START;
         newState.state[n.empty] = newState.chargrid[n.empty] = BLUE;
             // found it!
-        if( newState.chargrid == goalstring )
+        if( newState.chargrid == goalstring ) {
+          PrintGrid( newState.chargrid );
           break;
+        }
         if( !lookup.isThere(newState.chargrid) ) {
-          std::cout << "3rd" << std::endl;
           lookup.insertEntry(newState.chargrid);
           PrintGrid( newState.chargrid );
           s.push(newState);
@@ -216,31 +221,28 @@ void dfs()
           newState.state[n.empty+2] = newState.chargrid[n.empty+2] = START;
           newState.state[n.empty] = newState.chargrid[n.empty] = BLUE;
             // found it!
-          if( newState.chargrid == goalstring )
+          if( newState.chargrid == goalstring ) {
+            PrintGrid( newState.chargrid );
             break;
+          }
           if( !lookup.isThere(newState.chargrid) ) {
-            std::cout << "4th" << std::endl;
             lookup.insertEntry(newState.chargrid);
             PrintGrid( newState.chargrid );
             s.push(newState);
           }
         }
       
-      std::cout << "trying 5 and 6" << std::endl;
-      std::cout << "empty: " << n.empty << " size: " << size << std::endl;
-      std::cout << "n.empty - size: " << (n.empty - size) << std::endl;
-      if (( n.empty - size ) >= 0 ) { std::cout << "5-1 passes" << std::endl;}
-      if (( n.state[n.empty-size] == RED)) { std::cout << "5-2 passes" << std::endl;}
       if ((n.empty - size >= 0) && (n.state[n.empty-size] == RED)) {
         // make a new state up
         Node newState(n.state, n.chargrid, n.empty-size, false );
         newState.state[n.empty-size] = newState.chargrid[n.empty-size] = START;
         newState.state[n.empty] = newState.chargrid[n.empty] = RED;
             // found it!
-        if( newState.chargrid == goalstring )
+        if( newState.chargrid == goalstring ) {
+          PrintGrid( newState.chargrid );
           break;
+        }
         if( !lookup.isThere(newState.chargrid) ) {
-          std::cout << "5th" << std::endl;
           lookup.insertEntry(newState.chargrid);
           PrintGrid( newState.chargrid );
           s.push(newState);
@@ -252,27 +254,28 @@ void dfs()
           newState.state[n.empty-size*2] = newState.chargrid[n.empty-size*2] = START;
           newState.state[n.empty] = newState.chargrid[n.empty] = RED;
             // found it!
-          if( newState.chargrid == goalstring )
+          if( newState.chargrid == goalstring ) {
+            PrintGrid( newState.chargrid );
             break;
+          }
           if( !lookup.isThere(newState.chargrid) ) {
-            std::cout << "6th" << std::endl;
             lookup.insertEntry(newState.chargrid);
             PrintGrid( newState.chargrid );
             s.push(newState);
           }
         }
 
-      std::cout << "trying 7 and 8" << std::endl;
       if ((n.empty + size < size2) && (n.state[n.empty+size] == BLUE)) {
         // make a new state down
         Node newState(n.state, n.chargrid, n.empty+size, false );
         newState.state[n.empty+size] = newState.chargrid[n.empty+size] = START;
         newState.state[n.empty] = newState.chargrid[n.empty] = BLUE;
             // found it!
-        if( newState.chargrid == goalstring )
+        if( newState.chargrid == goalstring ) {
+          PrintGrid( newState.chargrid );
           break;
+        }
         if( !lookup.isThere(newState.chargrid) ) {
-          std::cout << "7th" << std::endl;
           lookup.insertEntry(newState.chargrid);
           PrintGrid( newState.chargrid );
           s.push(newState);
@@ -284,10 +287,11 @@ void dfs()
         newState.state[n.empty+size*2] = newState.chargrid[n.empty+size*2] = START;
         newState.state[n.empty] = newState.chargrid[n.empty] = BLUE;
             // found it!
-          if( newState.chargrid == goalstring )
+          if( newState.chargrid == goalstring ) {
+            PrintGrid( newState.chargrid );
             break;
+          }
           if( !lookup.isThere(newState.chargrid) ) {
-            std::cout << "8th" << std::endl;
             lookup.insertEntry(newState.chargrid);
             PrintGrid( newState.chargrid );
             s.push(newState);
@@ -311,5 +315,3 @@ void PrintGrid( std::string grid )
   }
   std::cout << std::endl << std::endl;
 }
-
-
